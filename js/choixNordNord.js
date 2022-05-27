e3e2fab498b943408d19fa4d1d4925df0668c343
JoanV` <joan.vigne@hotmail.fr`>
2022-05-27 16:52:43 +0100

    fin de journée

--- js/choixNordNord.js
+++ js/choixNordNord.js
@@ -1,13 +1,13 @@
 
 // CHOIX NORD NORD
-    // sans les fleches 
-    function disparaitreFleche() {
-        nord.classList.remove("hidden");
-        sud.classList.remove("hidden");
-        est.classList.remove("hidden");
-        ouest.classList.remove("hidden");
-        choixNordNord.style.marginTop = "20px";  
-    };
+// sans les fleches 
+function disparaitreFleche() {
+    nord.classList.remove("hidden");
+    sud.classList.remove("hidden");
+    est.classList.remove("hidden");
+    ouest.classList.remove("hidden");
+    choixNordNord.style.marginTop = "20px";
+};
 let nordNordDone = localStorage.getItem("choixNordNord")
 let choixNordNord = document.getElementById("choixNordNord");
 let choixNordNord1 = document.getElementById("choixNordNord1");
@@ -25,20 +25,21 @@ window.onload = function done() {
     if (nordNordDone === "choixNordNord2") {
         if (race === "orc") {
             paraChoix.innerHTML = "En vous rapprochant pour les saluer, vous vous rendez compte que ce sont <strong>des humains.</strong><br> Quand ils se rendent compte que vous êtes <strong>un orc</strong>,<br> ils prennent leurs jambes à leurs cous <strong>vers l'est</strong>  !";
-            if (race === "humain") {
-                paraChoix.innerHTML = "Au moment ou vous vous approchez pour les saluer, vous vous rendez compte que ce sont des humains.<br> Ils sont ravis de vous voir et vous retourne votre bonjour.";
-            }
-            if (race === "savana") {
-                alert("They re gringos, you scare them so they run away");
-            }
-            localStorage.setItem("choixNordNord", "choixNordNord2");
-            choixNordNord1.remove();
-            choixNordNord2.remove();
-            disparaitreFleche();
         }
+        if (race === "humain") {
+            paraChoix.innerHTML = "Vous les saluez,ils sont ravis de vous voir et vous retourne votre bonjour.";
+        }
+        if (race === "savana") {
+            alert("They re gringos, you scare them so they run away");
+        }
+        localStorage.setItem("choixNordNord", "choixNordNord2");
+        choixNordNord1.remove();
+        choixNordNord2.remove();
+        disparaitreFleche();
     }
-   
 }
+
+
 // stock le choix
 choixNordNord1.addEventListener("click", setChoixNordNord1);
 choixNordNord2.addEventListener("click", setChoixNordNord2);
