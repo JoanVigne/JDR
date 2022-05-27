e7350000a2b0091edd19d6a286ceaaf0d8ee7a89
JoanV` <joan.vigne@hotmail.fr`>
2022-05-27 15:19:20 +0100

    propre loup local storage

--- js/loup.js
+++ js/loup.js
@@ -18,7 +18,7 @@ function alertLoup() {
     if (pvLoup <= 0) {
         loupContainer.innerHTML = '<p>Vous avez vaincu<br>ce canidé sauvage.</p><br>';
         disparaitreFleche();
-        if(fourrure === "non"){
+        if (fourrure === "non") {
             loupContainer.insertAdjacentHTML('beforeend', '<div id="questionNNNN"><input type="button" value="Se vétire de sa fourrure" id="peauDeBete" class="choix"></div>');
             let peauDeBete = document.getElementById("peauDeBete");
             function setPeauDeBete() {
@@ -49,34 +49,11 @@ function rollTheDice() {
     playerInfo.innerHTML = "<img src='../images/perso/" + race + ".png' alt='personnage " + race + "'><br><h4> PV = " + PVPLAYER + "<br> Dégats = " + DEGATSPLAYER + "</h4><br>";
     pvLoup = pvLoup - dammagePlayer;
     loupContainer.innerHTML = "<img src='../images/perso/loup.png' alt=''> <br><h4> PV = " + pvLoup + "<br> Dégats  =" + degatsLoup + "</h4>";
+
+    // mettre a jour localstorage Loup 0 PV
     if (pvLoup <= 0) {
         localStorage.setItem("PVPLAYER", PVPLAYER);
-        let loups = [{
-            "nom": "NNNN",
-            "PV": 0,
-            "degats": 2
-        },
-        {
-            "nom": "NNNNN",
-            "PV": 7,
-            "degats": 3
-        },
-        {
-            "nom": "NNNO",
-            "PV": 4,
-            "degats": 3
-        },
-        {
-            "nom": "NNOO",
-            "PV": 5,
-            "degats": 2
-        },
-        {
-            "nom": "NNNOO",
-            "PV": 7,
-            "degats": 3
-        }
-        ]
+        loup.PV = 0;
         localStorage.setItem("loups", JSON.stringify(loups));
         window.location.reload();
     }
